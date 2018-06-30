869242192c1271e2d2ab67afc4393558a76574c1
Anatoliy <anatoliy.rus@gmail.com>
2018-06-30 15:59:13 +0300

    закончил доп задание 3

---
+++ lesson_2/task_3.php
@@ -0,0 +1,21 @@
+<?php
+function add($a, $b){
+    return $a + $b;
+}
+
+function sub($a, $b){
+    return $a - $b;
+}
+
+function mult($a, $b){
+    return $a * $b;
+}
+
+function divis($a, $b){
+    return $a / $b;
+}
+
+echo add(8, 4), '<br>';
+echo sub(8, 4), '<br>';
+echo mult(8, 4), '<br>';
+echo divis(8, 4), '<br>';
