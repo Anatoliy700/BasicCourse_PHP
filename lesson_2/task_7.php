c4759d962d40c13afdd6ad5482019d1db5b56da4
Anatoliy <anatoliy.rus@gmail.com>
2018-07-01 11:13:28 +0300

    правка по замечанию преподавателя

--- lesson_2/task_7.php
+++ lesson_2/task_7.php
@@ -1,19 +1,24 @@
 <?php
 function getNowTime() {
-  $hours = date('H');
-  $minutes = date('i');
-  
+  $time = time();
+  $hours = date('H', $time);
+  $minutes = date('i', $time);
   
   return $hours . ' ' . getCorrectVariant($hours, 'час', 'часа', 'часов') . ' '
     . $minutes . ' ' . getCorrectVariant($minutes, 'минута', 'минуты', 'минут');
 }
 
 function getCorrectVariant($num, $var1, $var2, $var3) {
-  $num = (int)$num;
-  $lastNum = $num % 10;
-  if (($num > 10 && $num < 21) || $lastNum === 0 || ($lastNum >= 5 && $lastNum <= 9)) return $var3;
-  elseif ($lastNum === 1) return $var1;
-  else return $var2;
+  if ($num > 20) {
+    $num %= 10;
+  }
+  if ($num > 1 && $num < 5) {
+    return $var2;
+  } elseif ($num === 1) {
+    return $var1;
+  } else {
+    return $var3;
+  }
 }
 
 echo getNowTime(), '<br>';
