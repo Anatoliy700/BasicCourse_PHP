79fe3a0aabb3b131cfc40aad2ed12c95898947fb
Anatoliy <anatoliy.rus@gmail.com>
2018-07-18 20:29:47 +0300

    Оптимизировал корзину

--- lesson_7/engine/productsFunctions.php
+++ lesson_7/engine/productsFunctions.php
@@ -40,3 +40,95 @@ function getProductsById($param) {
               WHERE p.id in ({$idStr}) ";
   return dbQueryGetAll($query);
 }
+
+/**
+ * @param $id
+ * @param $amount
+ * @param $type
+ * @return bool
+ */
+function addOrRemoveProductInCart($id, $amount, $type) {
+  switch ($type) {
+    
+    //если запрос на изменение количества товара в корзине
+    case 'update':
+      $update = true;
+    
+    //если запрос на добавление товара в корзину
+    case 'add':
+      $product_id = (int)$id;
+      $product_amount = (int)$amount;
+      if ($product_id && $product_amount) {
+        $arrKey = false;
+        
+        //если в корзине уже есть товар
+        if ($_SESSION['cart']) {
+          foreach ($_SESSION['cart'] as $key => $item) {
+            //проверяем нет ли уже добавляемого товара в карзине и если есть то только увеличиваем количество
+            if ($item['id'] === $product_id) {
+              $arrKey = $key;
+              break;
+            }
+          }
+        }
+        //если добавляемые товар уже присутсвует в карзине
+        if ($arrKey !== false) {
+          
+          //если пришел запрос на обновление количества
+          if ($update) {
+            $_SESSION['cart'][$arrKey]['amount'] = $product_amount;
+          } else {
+            $_SESSION['cart'][$arrKey]['amount'] += $product_amount;
+          }
+          //если добавляемые товар не присутсвует в карзине
+        } else {
+          $_SESSION['cart'][] = ['id' => $product_id, 'amount' => $product_amount];
+        }
+        return true;
+      }
+      break;
+    //если пришел запрос на удаление товара из карзины
+    case 'remove':
+      $product_id = (int)$_POST['id'];
+      if ($product_id) {
+        foreach ($_SESSION['cart'] as $key => $item) {
+          if ($item['id'] === $product_id) {
+            array_splice($_SESSION['cart'], $key, 1);
+            break;
+          }
+        }
+        return true;
+      }
+    
+  }
+  return false;
+}
+
+
+function getProductOfCart($arrProductCart) {
+  $out = false;
+  $arrId = [];
+  foreach ($arrProductCart as $product) {
+    if ($product['id'] !== null) {
+      $arrId[] = $product['id'];
+    }
+  }
+  //если удалось собрать ID товаров в корзине то делаем запрос к БД для получения детальной информации по ним
+  if ($arrId) {
+    $fullCartProducts = $arrProductCart;
+    $cartProductsDb = getProductsById($arrId);
+    $totalPriceCart = 0;
+    foreach ($fullCartProducts as $key => $item) {
+      foreach ($cartProductsDb as $productDb) {
+        if ($item['id'] == $productDb['id']) {
+          $fullCartProducts[$key]['details'] = $productDb;
+          $fullCartProducts[$key]['total_price'] = $productDb['price'] * $item['amount'];
+          $totalPriceCart += $fullCartProducts[$key]['total_price'];
+        }
+      }
+    }
+    $out['totalPrice'] = $totalPriceCart;
+    $out['products'] = $fullCartProducts;
+  }
+  return $out;
+}
--- lesson_7/public/cart.php
+++ lesson_7/public/cart.php
@@ -5,59 +5,10 @@ include CONFIG_DIR . 'includeLinks.php';
 
 session_start();
 
-// если запрос на добавление или обновление товара в карзине
+// если запрос на добавление, обновление или удаление товара в карзине
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['type']) {
-  switch ($_POST['type']) {
-    
-    //если запрос на изменение количества товара в корзине
-    case 'update':
-      $update = true;
-    
-    //если запрос на добавление товара в корзину
-    case 'add':
-      $product_id = (int)$_POST['id'];
-      $product_amount = (int)$_POST['amount'];
-      if ($product_id && $product_amount) {
-        $arrKey = false;
-        
-        //если в корзине уже есть товар
-        if ($_SESSION['cart']) {
-          foreach ($_SESSION['cart'] as $key => $item) {
-            //проверяем нет ли уже добавляемого товара в карзине и если есть то только увеличиваем количество
-            if ($item['id'] === $product_id) {
-              $arrKey = $key;
-              break;
-            }
-          }
-        }
-        //если добавляемые товар уже присутсвует в карзине
-        if ($arrKey !== false) {
-          
-          //если пришел запрос на обновление количества
-          if ($update) {
-            $_SESSION['cart'][$arrKey]['amount'] = $product_amount;
-          } else {
-            $_SESSION['cart'][$arrKey]['amount'] += $product_amount;
-          }
-          //если добавляемые товар не присутсвует в карзине
-        } else {
-          $_SESSION['cart'][] = ['id' => $product_id, 'amount' => $product_amount];
-        }
-      }
-      break;
-    
-    //если пришел запрос на удаление товара из карзины
-    case 'remove':
-      $product_id = (int)$_POST['id'];
-      if ($product_id) {
-        foreach ($_SESSION['cart'] as $key => $item) {
-          if ($item['id'] === $product_id) {
-            array_splice($_SESSION['cart'], $key, 1);
-            break;
-          }
-        }
-      }
-  }
+  
+  addOrRemoveProductInCart($_POST['id'], $_POST['amount'], $_POST['type']);
 }
 
 //если пользователь авторизован то выводим в глобальном шаблоне ссылку на выход иначе выводим ссылку на вход
@@ -69,31 +20,14 @@ if (checkAuthUsers()) {
 
 //если у пользователя есть товары в карзине то выводим их на странице
 if ($_SESSION['cart'] && count($_SESSION['cart'])) {
-  $arrId = [];
-  foreach ($_SESSION['cart'] as $product) {
-    if ($product['id'] !== null) {
-      $arrId[] = $product['id'];
-    }
-  }
-  //если удалось собрать ID товаров в корзине то делаем запрос к БД для получения детальной информации по ним
-  if ($arrId) {
-    $fullCartProducts = $_SESSION['cart'];
-    $cartProductsDb = getProductsById($arrId);
-    $totalPriceCart = 0;
-    foreach ($fullCartProducts as $key => $item) {
-      foreach ($cartProductsDb as $productDb) {
-        if ($item['id'] == $productDb['id']) {
-          $fullCartProducts[$key]['details'] = $productDb;
-          $fullCartProducts[$key]['total_price'] = $productDb['price'] * $item['amount'];
-          $totalPriceCart += $fullCartProducts[$key]['total_price'];
-        }
-      }
-    }
-    
+  
+  $productOfCart = getProductOfCart($_SESSION['cart']);
+  
+  if ($productOfCart) {
     //выводим шаблон с товарами пользователя
     render('cart-template', [
-      'products' => $fullCartProducts,
-      'totalPriceCart' => $totalPriceCart
+      'products' => $productOfCart['products'],
+      'totalPriceCart' => $productOfCart['totalPrice']
     ], [
       'template' => 'main',
       'options' => $layoutOptions
